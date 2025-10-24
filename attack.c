8fb43c2bfe3abcfe163fd349e9e0ee346fd9d8dc
Loyst <loui.coat@gmail.com>
2025-10-24 23:04:21 +0200

    Cleaning code a bit

--- attack.c
+++ attack.c
@@ -8,6 +8,22 @@
 
 /************************************************************/
 /*
+ * 					Random Key Generation
+ */
+/************************************************************/
+
+/**
+ * @brief
+ * Generates a random key from /dev/urandom
+ */
+void generate_random_key(uint8_t key[AES_128_KEY_SIZE]) {
+    int fd = open("/dev/urandom", O_RDONLY);
+    read(fd, key, AES_128_KEY_SIZE);
+    close(fd);
+}
+
+/************************************************************/
+/*
  * 						Print helpers
  */
 /************************************************************/
@@ -26,6 +42,16 @@ void print_key(uint8_t key[AES_128_KEY_SIZE]) {
 	printf("\n");
 }
 
+/************************************************************/
+/*
+ * 			AES Encryption/Decryption and KeySchedule
+ */
+/************************************************************/
+
+/**
+ * @brief
+ * Performs the inverse operation of ShiftRows, which was in fact not necessary for the attack
+ */
 void invert_shift_rows(uint8_t block[AES_BLOCK_SIZE]) {
 	uint8_t tmp;
 	/*
@@ -34,12 +60,14 @@ void invert_shift_rows(uint8_t block[AES_BLOCK_SIZE]) {
 	
 	 /* Row 0 */
 	// No changes
+
 	/* Row 1 */
 	tmp = block[13];
 	block[13] = block[ 9];
 	block[ 9] = block[ 5];
 	block[ 5] = block[ 1];
 	block[ 1] = tmp;
+	
 	/* Row 2 */
 	tmp = block[2];
 	block[ 2] = block[10];
@@ -47,6 +75,7 @@ void invert_shift_rows(uint8_t block[AES_BLOCK_SIZE]) {
 	tmp = block[6];
 	block[ 6] = block[14];
 	block[14] = tmp;
+	
 	/* Row 3 */
 	tmp = block[3];
 	block[ 3] = block[ 7];
@@ -56,29 +85,22 @@ void invert_shift_rows(uint8_t block[AES_BLOCK_SIZE]) {
 }
 
 /**
- * Performs the inverse of AES cipher for the last (half) round consisting of 
+ * @brief
+ * Performs the inverse of AES cipher for the last (half) round consisting of:
  * - SubBytes
- * - ShiftRows
+ * - ShiftRows (unnecessary)
  * - AddRoundKey
  */
 void invert_half_round(uint8_t block[AES_BLOCK_SIZE], uint8_t roundkey[AES_128_KEY_SIZE]) 
 {
-	// AddRoundKey
+	// AddRoundKey & SubBytes
 	for (size_t i = 0; i < AES_BLOCK_SIZE; i++) {
-		block[i] = block[i] ^ roundkey[i];
+		block[i] = Sinv[block[i] ^ roundkey[i]];
 	}
-
-	// InvShiftRows
-	// invert_shift_rows(block); // this is actually unnecessary for what we're doing here
-
-	// InvSubBytes
-	for (size_t i = 0; i < AES_BLOCK_SIZE; i++) {
-		block[i] = Sinv[block[i]];
-  	}
 }
 
 /**
- * Unused (other way of computing the key)
+ * Unused (other way of computing the key - byte per byte)
  */
 // int invert_half_round_per_byte(uint8_t block_byte, uint8_t key_byte) 
 // {
@@ -87,7 +109,7 @@ void invert_half_round(uint8_t block[AES_BLOCK_SIZE], uint8_t roundkey[AES_128_K
 
 /**
  * @brief
- * Performs the inverse opération of next round key 4 times (rounds 3, 2, 1, 0) for a given key 
+ * Performs the inverse operation of next round key 4 times (rounds 3, 2, 1, 0) for a given fourth round key 
  */
 void invert_key(uint8_t key[AES_128_KEY_SIZE]) {
 	uint8_t tmp_key[AES_128_KEY_SIZE];
@@ -95,17 +117,6 @@ void invert_key(uint8_t key[AES_128_KEY_SIZE]) {
 	prev_aes128_round_key(tmp_key, key, 2);
 	prev_aes128_round_key(key, tmp_key, 1);
 	prev_aes128_round_key(tmp_key, key, 0);
-}
-
-
-/**
- * @brief
- * Generates a random key from /dev/urandom
- */
-void generate_random_key(uint8_t key[AES_128_KEY_SIZE]) {
-    int fd = open("/dev/urandom", O_RDONLY);
-    read(fd, key, AES_128_KEY_SIZE);
-    close(fd);
 }
 
 /**
@@ -136,7 +147,11 @@ void gen_all_delta_sets(uint8_t ciphers[AES_BLOCK_SIZE][256][AES_BLOCK_SIZE], ui
 	}
 }
 
-
+/**
+ * @brief
+ * Performs the partial deciphering of the 256 ciphertexts of the delta-set (down to 3rd round) using the key guess. 
+ * Then compute the result block which consists of the xor of the 256 partially deciphers ciphertexts.
+ */
 void compute_distinguisher_for_key_guess(uint8_t ciphers[256][AES_BLOCK_SIZE], uint8_t key_guess[AES_128_KEY_SIZE], uint8_t result[AES_BLOCK_SIZE]) {
 	memset(result, 0, AES_BLOCK_SIZE);
 	// print_key(key_guess);
@@ -154,7 +169,7 @@ void compute_distinguisher_for_key_guess(uint8_t ciphers[256][AES_BLOCK_SIZE], u
 }
 
 /**
- * Unused (other way of computing the key)
+ * Unused (other way of computing the key - byte per byte)
  */
 // int compute_distinguisher_for_key_guess_byte(uint8_t ciphers[256][AES_BLOCK_SIZE], uint8_t key_guess[AES_128_KEY_SIZE], uint8_t i_byte) {
 // 	int acc = 0;
@@ -181,14 +196,12 @@ bool verify_result(uint8_t result[AES_BLOCK_SIZE], uint8_t delta_set) {
 }
 
 /**
- * @brief
- * Performs the whole attack.
+ * @brief Performs the whole square attack on 3 1/2 rounds of the AES block.
  * 
  * @param ciphers
  * The encrypted chosen plaintexts (the function relies on a first encryption and then may re-encrypt the plaintexts).
  * @param key
  * The actual key used to cipher the chose plaintexts.
- * 
  */
 void key_recovery_attack(uint8_t ciphers[AES_BLOCK_SIZE][256][AES_BLOCK_SIZE], uint8_t key_guess[AES_128_KEY_SIZE]) {
 	uint8_t ciphers_copy[AES_BLOCK_SIZE][256][AES_BLOCK_SIZE];
@@ -217,6 +230,11 @@ void key_recovery_attack(uint8_t ciphers[AES_BLOCK_SIZE][256][AES_BLOCK_SIZE], u
 
 			// Try all DELTA-sets (avoid false positives)
 			bool correct_key = true;
+			// Note that it could also be possible to compute the distinguisher on only one delta-set
+			// then possibly check additional delta-sets if there are several candidates for a byte
+			// It would arguably be faster than our current technique that will try (on average) 128
+			// values per byte of the key and therefore compute 16*128*16 distinguishers on average 
+			// (n_delta_sets * byte_value * n_bytes)  
 			for (size_t delta_set = 0; delta_set < AES_BLOCK_SIZE; delta_set++) {
 				compute_distinguisher_for_key_guess(ciphers[delta_set], key_guess, result);
 
